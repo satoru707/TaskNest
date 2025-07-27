73905cd74bc715e2207c818ac8e917e13cc69aac
satoru707 <petolulope7@gmail.com>
2025-07-27 13:42:29 +0100

    tn

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  `${import.meta.env.SOCKET_CORS_ORIGIN}/api` || "http://localhost:3000/api";
+const API_BASE_URL = import.meta.env.SOCKET_CORS_ORIGIN
+  ? `${import.meta.env.SOCKET_CORS_ORIGIN}/api`
+  : "http://localhost:3000/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
